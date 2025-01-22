cb8c2e3d817de07b9f761380889ea6dfb99330ac
generation21 <qbration21@gmail.com>
2025-01-22 16:32:27 +0900

    feat: project init

---
+++ lib/main.dart
@@ -0,0 +1,32 @@
+import 'package:flutter/material.dart';
+import 'package:tiktok_onboarding_flow/constants/sizes.dart';
+import 'package:tiktok_onboarding_flow/screens/features/authentication/signup_screen.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Onboarding Flow part',
+      theme: ThemeData(
+        scaffoldBackgroundColor: Colors.white,
+        primaryColor: const Color(0xFFE0435A),
+        appBarTheme: const AppBarTheme(
+          backgroundColor: Colors.white,
+          elevation: 0,
+          titleTextStyle: TextStyle(
+            fontSize: Sizes.size16 + Sizes.size2,
+            fontWeight: FontWeight.w600,
+            color: Colors.black,
+          ),
+        ),
+      ),
+      home: SignUpScreen(),
+    );
+  }
+}
