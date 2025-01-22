1ef9ae980666194fe93673ec1f025835ca13661d
generation21 <qbration21@gmail.com>
2025-01-22 23:10:25 +0900

    feat: enhance FormButton with active state and customizable colors for improved user interaction

--- lib/screens/features/authentication/widgets/form_button.dart
+++ lib/screens/features/authentication/widgets/form_button.dart
@@ -4,17 +4,24 @@ import 'package:tiktok_onboarding_flow/constants/sizes.dart';
 class FormButton extends StatelessWidget {
   final String text;
   final VoidCallback onPressed;
+  final Color foregroundColor;
+  final Color backgroundColor;
+
+  final bool isActive;
 
   const FormButton({
     super.key,
     required this.text,
     required this.onPressed,
+    required this.isActive,
+    this.foregroundColor = Colors.white,
+    this.backgroundColor = const Color(0xFF101418),
   });
 
   @override
   Widget build(BuildContext context) {
     return GestureDetector(
-      onTap: onPressed,
+      onTap: isActive ? onPressed : null,
       child: FractionallySizedBox(
         widthFactor: 1,
         child: Container(
@@ -23,12 +30,12 @@ class FormButton extends StatelessWidget {
             horizontal: Sizes.size24,
           ),
           decoration: BoxDecoration(
-              color: Color(0xFF101418),
+              color: isActive ? backgroundColor : Colors.grey.shade300,
               borderRadius: BorderRadius.circular(Sizes.size40)),
           child: Text(text,
               textAlign: TextAlign.center,
               style: TextStyle(
-                  color: Colors.white,
+                  color: isActive ? foregroundColor : Colors.grey.shade800,
                   fontWeight: FontWeight.w900,
                   fontSize: Sizes.size20)),
         ),
