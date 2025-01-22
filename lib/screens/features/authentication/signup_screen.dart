aebc6d3d29fd10a897ad454d186a5e9955e8e1ac
generation21 <qbration21@gmail.com>
2025-01-22 18:45:16 +0900

    feat: implement signup screen with social authentication options and navigation

--- lib/screens/features/authentication/signup_screen.dart
+++ lib/screens/features/authentication/signup_screen.dart
@@ -1,10 +1,172 @@
+import 'package:flutter/gestures.dart';
 import 'package:flutter/material.dart';
+import 'package:font_awesome_flutter/font_awesome_flutter.dart';
+import 'package:tiktok_onboarding_flow/constants/gaps.dart';
+import 'package:tiktok_onboarding_flow/constants/sizes.dart';
+import 'package:tiktok_onboarding_flow/screens/features/authentication/login_screen.dart';
+import 'package:tiktok_onboarding_flow/screens/features/authentication/signup_form_screen.dart';
+import 'package:tiktok_onboarding_flow/screens/features/authentication/widgets/form_button.dart';
+import 'package:tiktok_onboarding_flow/screens/features/authentication/widgets/social_auth_button.dart';
 
 class SignUpScreen extends StatelessWidget {
   const SignUpScreen({super.key});
 
+  void _onCreateAccountPressed(BuildContext context) {
+    Navigator.of(context).push(
+      MaterialPageRoute(
+        builder: (context) => const SignUpFormScreen(),
+      ),
+    );
+  }
+
+  void _onLogInPressed(BuildContext context) {
+    Navigator.of(context).push(
+      MaterialPageRoute(
+        builder: (context) => const LoginScreen(),
+      ),
+    );
+  }
+
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return Scaffold(
+      appBar: AppBar(
+        title: FaIcon(FontAwesomeIcons.twitter,
+            color: Theme.of(context).primaryColor, size: Sizes.size36),
+      ),
+      body: Padding(
+        padding: const EdgeInsets.symmetric(horizontal: Sizes.size40),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.center,
+          children: [
+            Gaps.v80,
+            Text(
+              'See what\'s happening in the world right now.',
+              style: TextStyle(
+                fontWeight: FontWeight.w900,
+                fontSize: Sizes.size28,
+              ),
+            ),
+            Gaps.v24,
+            SocialAuthButton(
+              icon: FaIcon(FontAwesomeIcons.google),
+              text: "Continue with Google",
+              onPressed: () {},
+            ),
+            Gaps.v16,
+            SocialAuthButton(
+              icon: FaIcon(FontAwesomeIcons.apple),
+              text: "Continue with Apple",
+              onPressed: () {},
+            ),
+            Gaps.v16,
+            Row(
+              children: [
+                Expanded(
+                  child: Divider(
+                    color: Colors.grey.shade400,
+                    thickness: 1,
+                  ),
+                ),
+                Padding(
+                  padding: const EdgeInsets.symmetric(horizontal: Sizes.size16),
+                  child: Text(
+                    'or',
+                    style: TextStyle(
+                      color: Colors.grey.shade600,
+                      fontWeight: FontWeight.w600,
+                    ),
+                  ),
+                ),
+                Expanded(
+                  child: Divider(
+                    color: Colors.grey.shade400,
+                    thickness: 1,
+                  ),
+                ),
+              ],
+            ),
+            Gaps.v16,
+            FormButton(
+              text: "Create account",
+              onPressed: () => _onCreateAccountPressed(context),
+            )
+          ],
+        ),
+      ),
+      bottomNavigationBar: BottomAppBar(
+        height: Sizes.size96 * 2,
+        color: Colors.white,
+        elevation: 0,
+        child: Padding(
+          padding: const EdgeInsets.symmetric(horizontal: Sizes.size40),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              RichText(
+                text: TextSpan(
+                  style: TextStyle(
+                    color: Colors.black,
+                    fontSize: Sizes.size16,
+                  ),
+                  children: [
+                    const TextSpan(
+                      text: "By signing up, you agree to our ",
+                    ),
+                    TextSpan(
+                      text: "Terms",
+                      style: TextStyle(
+                        color: Theme.of(context).primaryColor,
+                        fontWeight: FontWeight.w600,
+                      ),
+                    ),
+                    const TextSpan(text: ", "),
+                    TextSpan(
+                      text: "Privacy Policy",
+                      style: TextStyle(
+                        color: Theme.of(context).primaryColor,
+                        fontWeight: FontWeight.w600,
+                      ),
+                    ),
+                    const TextSpan(text: ", and "),
+                    TextSpan(
+                      text: "Cookie Use",
+                      style: TextStyle(
+                        color: Theme.of(context).primaryColor,
+                        fontWeight: FontWeight.w600,
+                      ),
+                    ),
+                    const TextSpan(text: "."),
+                  ],
+                ),
+              ),
+              Gaps.v16,
+              RichText(
+                text: TextSpan(
+                  style: TextStyle(
+                    color: Colors.black,
+                    fontSize: Sizes.size16,
+                  ),
+                  children: [
+                    TextSpan(
+                      text: "Already have an account? ",
+                    ),
+                    TextSpan(
+                      text: "Log in",
+                      style: TextStyle(
+                        color: Theme.of(context).primaryColor,
+                        fontWeight: FontWeight.w600,
+                      ),
+                      recognizer: TapGestureRecognizer()
+                        ..onTap = () => _onLogInPressed(context),
+                    ),
+                  ],
+                ),
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
   }
 }
